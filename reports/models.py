721a082b7f3ee34949b31202d677093a6fae96e6
amirkhadari <amirkhadari@gmail.com>
2021-04-26 18:02:41 +0530

    Added reports model in the reports app

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from profiles.models import Profile
 
 # Create your models here.
+
+
+class Report(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='reports/', blank=True)
+    remarks = RichTextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
